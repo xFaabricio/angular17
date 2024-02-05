e6d36044199ec410bcd50e681c4107e9ce1e15c8
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-02-04 23:41:16 -0300

    Rollback

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,3 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 
-export const routes: Routes = [
-    { path: '', redirectTo: '/pt-BR', pathMatch: 'full' },
-    { path: '/', component: AppComponent },
-];
+export const routes: Routes = [];
