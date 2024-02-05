10bf24c623c2d9ebd1532634ee9f356ec50f2188
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-02-05 00:04:41 -0300

    More one try

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/pt-BR', pathMatch: 'full' },
+    { path: 'pt-BR', component: AppComponent },
+    { path: 'en-US', component: AppComponent },
+  ];
