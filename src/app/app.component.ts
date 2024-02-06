c89c65970666ddeb8f91026346a7c3bc201e2a74
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-02-06 19:40:49 -0300

    Header test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule],
+  imports: [RouterOutlet, NgbModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
