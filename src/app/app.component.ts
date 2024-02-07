533c941f6a3f658b70c54064904977272f56c617
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-02-06 22:35:56 -0300

    Salvando Progresso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-root',
@@ -11,5 +12,32 @@ import { HeaderComponent } from './header/header.component';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular17';
+  title = 'SevenHeads Project';
+
+  darkMode: boolean = false;
+
+  constructor(private cookieService: CookieService) {    
+    if (this.cookieService.check('darkMode')) {      
+      this.darkMode = this.cookieService.get('darkMode') === 'true';
+    } else {      
+      this.cookieService.set('darkMode', this.darkMode.toString());
+    }
+    this.changeTheme();
+  }
+
+  updateDarkMode(event: any): void {
+    if (event.target instanceof HTMLInputElement) {
+      const checked = event.target.checked;
+      this.darkMode = checked;
+      this.cookieService.set('darkMode', this.darkMode.toString());
+      this.changeTheme();
+    }
+  }
+
+  changeTheme(): void {
+    if (typeof window !== 'undefined') {
+      const element = document.body as HTMLElement;
+      element.dataset['bsTheme'] = this.darkMode ? "dark" : "light";
+    }
+  }
 }
