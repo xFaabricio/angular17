65e70de03a44a7d39cab4367ad62d5e39593c644
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-04-28 15:27:27 -0300

    Sakai Home

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { DividerModule } from 'primeng/divider';
+import { StyleClassModule } from 'primeng/styleclass';
+import { ChartModule } from 'primeng/chart';
+import { PanelModule } from 'primeng/panel';
+import { ButtonModule } from 'primeng/button';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-home-content',
   standalone: true,
-  imports: [],
+  imports: [DividerModule,
+            StyleClassModule,            
+            PanelModule,
+            ButtonModule,
+            CommonModule],
   templateUrl: './home-content.component.html',
   styleUrl: './home-content.component.scss'
 })
