f3b1f68b07d324c0241248886af15fbf80f1e0e3
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-04-28 13:04:29 -0300

    Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header/header.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgbModule, HeaderComponent],
+  imports: [RouterOutlet, NgbModule, HeaderComponent, HomeContentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/home-content/home-content.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-content.component.html',
+  styleUrl: './home-content.component.scss'
+})
+export class HomeContentComponent {
+
+}
