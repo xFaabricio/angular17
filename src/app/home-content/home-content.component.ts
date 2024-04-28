7d38405db5c32bce595158c0ee30543b84e96c0b
Fabrício Oliveira <fabricio_oliveira1@hotmail.com>
2024-04-28 18:55:39 -0300

    Change theme

--- src/app/home-content/home-content.component.ts
+++ src/app/home-content/home-content.component.ts
@@ -4,9 +4,11 @@ import { StyleClassModule } from 'primeng/styleclass';
 import { PanelModule } from 'primeng/panel';
 import { ButtonModule } from 'primeng/button';
 import { CommonModule } from '@angular/common';
-import { LayoutService } from '../service/app.layout.service';
 import { AppComponent } from '../app.component';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faCloudMoon } from '@fortawesome/free-solid-svg-icons';
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home-content',
@@ -15,13 +17,18 @@ import { AppComponent } from '../app.component';
             StyleClassModule,            
             PanelModule,
             ButtonModule,
-            CommonModule],
+            CommonModule,
+            FontAwesomeModule],
   templateUrl: './home-content.component.html',
   styleUrl: './home-content.component.scss'
 })
 export class HomeContentComponent {
   
   constructor(public appComponent: AppComponent) { }
+
+  faSun = faSun;
+  faMoon = faMoon;
+  faCloudMoon = faCloudMoon;
 
   changeTheme(theme: string, colorScheme: string) {
     this.appComponent.changeTheme(theme, colorScheme);
